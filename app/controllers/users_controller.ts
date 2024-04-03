5b149eda4930177d2a489d9aaad966046e5d8b2b
Carlos Leal <carlos.frleal@gmail.com>
2024-04-03 00:11:12 -0300

    feat: login route

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,6 +1,8 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 import User from '../models/user.js';
+import hash from '@adonisjs/core/services/hash'
+
 
 export default class UsersController {
   async signup({request, response}: HttpContext) {
@@ -15,6 +17,31 @@ export default class UsersController {
     } catch (error: any) {
       console.error(error.code)
       const errorMessage = error.code === 'ER_DUP_ENTRY' ? 'Usuário já existente' : error.message;
+      return {
+        message: errorMessage,
+      };   
+    }
+  }
+
+  async login({request, response}: HttpContext) {
+    try {
+      const { email, password } = request.all();
+      const user = await User.findByOrFail('email', email);
+
+      const passwordVerified = await hash.verify(user.password, password);
+
+      if (!passwordVerified) {        
+        return response.status(401).send({ error: 'Senha inválida' });
+      }
+      const token = await User.accessTokens.create(user);
+
+      return {
+        type: 'bearer',
+        value: token.value!.release(),
+      };
+    } catch (error: any) {
+      console.error(error.code)
+      const errorMessage = error.code === 'E_ROW_NOT_FOUND' ? 'Usuário não encontrado' : error.message;
       return {
         message: errorMessage,
       };   
