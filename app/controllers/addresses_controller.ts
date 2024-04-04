a16c49c84f01ea40275c5200df1ba169650ff8fe
Carlos Leal <carlos.frleal@gmail.com>
2024-04-03 21:35:11 -0300

    feat: created routes for delete and update an address

--- app/controllers/addresses_controller.ts
+++ app/controllers/addresses_controller.ts
@@ -33,4 +33,45 @@ export default class AddressesController {
       return response.status(500).send({ message: 'Erro interno do servidor' });
     }
   }
+
+  async update({params, request, response}: HttpContext) {
+    try {
+      const body = request.body();
+      const address = await Address.findOrFail(params.id);
+      
+      address.street = body.street;
+      address.number = body.number;
+      address.neighborhood = body.neighborhood;
+      address.city = body.city;
+      address.state = body.state;
+      address.country = body.country;
+      address.postal_code = body.postal_code;
+
+      await address.save();
+
+      return response.status(200).send({ data: address });
+    } catch(error: any) {
+      console.error(error.message);
+      if (error.message === 'Row not found') {
+        return response.status(200).send({ message: 'Endereço não encontrado' });
+      }
+      return response.status(500).send({ message: 'Erro interno do servidor' });
+    }
+  }
+
+  async destroy({params, response}: HttpContext) {
+    try {
+      const address = await Address.findOrFail(params.id);
+
+      await address.delete();
+      
+      return response.status(204);
+    } catch(error: any) {
+      console.error(error.message);
+      if (error.message === 'Row not found') {
+        return response.status(200).send({ message: 'Endereço não encontrado' });
+      }
+      return response.status(500).send({ message: 'Erro interno do servidor' });
+    }
+  }
 }
