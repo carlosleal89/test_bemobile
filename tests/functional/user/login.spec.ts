91b70f3c73bb314c69f56181a5485cbf0cfdfcfe
Carlos Leal <carlos.frleal@gmail.com>
2024-04-05 00:39:20 -0300

    wip: tests for users

---
+++ tests/functional/user/login.spec.ts
@@ -0,0 +1,36 @@
+import { test } from '@japa/runner';
+
+import User from '../../../app/models/user.js';
+import hash from '@adonisjs/core/services/hash';
+
+test.group('Testes para criação e login de usuário: ', (group) => {
+  let user: any;
+  group.each.setup(async () => {
+    user = new User();
+    user.password = 'stars';
+    user.fullName = 'Nemesis';
+    user.email = 'nemesis@umbrella.com'
+    await user.save();
+  })
+
+  group.each.teardown(async () => {
+    await user.delete();
+  })
+  test('Testa se a função de criptografa a senha funciona: ', async ({ assert }) => {
+    assert.isTrue(hash.isValidHash(user.password));
+    assert.isTrue(await hash.verify(user.password, 'stars'));
+  });
+
+  test('Testa se a função de criptografar retorna um erro com a senha incorreta: ', async ({ assert }) => {
+    assert.isFalse(await hash.verify(user.password, 'jill'));
+  });
+
+  // test('Testa se o sistema retorna um erro ao tentar cadastrar um usuário com o mesmo email: ', async ({ assert, expect }) => {
+  //   user.password = 'stars';
+  //   user.fullName = 'Chris Redfield';
+  //   user.email = 'nemesis@umbrella.com'
+  //   await user.save();
+
+  //   expect
+  // })
+})
