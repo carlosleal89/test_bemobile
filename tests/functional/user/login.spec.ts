f955b0419e4fd86fc936ae5ca141fd4158f3823b
Carlos Leal <carlos.frleal@gmail.com>
2024-04-05 21:49:57 -0300

    feat: tests for some routes of clients and products

--- tests/functional/user/login.spec.ts
+++ tests/functional/user/login.spec.ts
@@ -1,5 +1,5 @@
 import { test } from '@japa/runner';
-import { apiClient } from '@japa/api-client';
+// import { apiClient } from '@japa/api-client';
 import User from '../../../app/models/user.js';
 import hash from '@adonisjs/core/services/hash';
 
@@ -34,18 +34,134 @@ test.group('Testes para criação e login de usuário: ', (group) => {
     
     const response = await client.post('/signup').json(userData);
     response.assertStatus(409);
-  });
+  });  
+})
 
-  // test('Testa se o sistema cadastra um novo usuário: ', async ({ client }) => {
-  //   const userData = {
-  //     password: 'stars',
-  //     fullName: 'Chris Redfield',
-  //     email: 'tesdte@stars.com',
-  //   }
+test.group('Testes para as rotas de clientes: ', (group) => {
+  let user: any;
+  group.each.setup(async () => {
+    user = await User.create({
+      fullName: 'testuser',
+      email: 'test@example.com',
+      password: 'password'
+    });
+  })
+
+  group.each.teardown(async () => {
+    await user.delete();
+  })
+
+  test('Testa se get /api/clients/ retorna o status 200: ', async ({ client }) => {
+    const loginResponse = await client
+    .post('/login')
+    .json({
+      email: 'test@example.com',
+      password: 'password'
+    })
     
-  //   const response = await client.post('/signup').json(userData);
-  //   console.log(response.body);
+    const token = loginResponse.body().token;
+
+    const response = await client
+    .get('/api/clients/')
+    .header('Authorization', `Bearer ${token}`)
+
+    response.assertStatus(200);
+  })
+
+  test('Testa se get /api/clients/:id retorna o status 200: ', async ({ client }) => {
+    const loginResponse = await client
+    .post('/login')
+    .json({
+      email: 'test@example.com',
+      password: 'password'
+    })
     
-  //   response.assertStatus(201);
-  // })
+    const token = loginResponse.body().token;
+
+    const response = await client
+    .get('/api/clients/1')
+    .header('Authorization', `Bearer ${token}`)
+
+    response.assertStatus(200);
+  })
+
+  test('Testa se get /api/clients/:id retorna o status 404 ao não encontrar um cliente: ', async ({ client }) => {
+    const loginResponse = await client
+    .post('/login')
+    .json({
+      email: 'test@example.com',
+      password: 'password'
+    })
+    
+    const token = loginResponse.body().token;    
+
+    const response = await client
+    .get('/api/clients/100')
+    .header('Authorization', `Bearer ${token}`)
+
+    response.assertStatus(404);
+    response.assertBody({ message: 'Cliente não encontrado' });
+  })
+
+  test('Testa se get /api/clients/ retorna o status 401 ao tentar fazer a requisição com um token invalido: ', async ({ client }) => {
+    await client
+    .post('/login')
+    .json({
+      email: 'test@example.com',
+      password: 'password'
+    })
+
+    const response = await client
+    .get('/api/clients/')
+    .header('Authorization', `Bearer umtokenqualquer`);
+
+    response.assertStatus(401);
+  })
+})
+
+test.group('Testes para as rotas de produtos: ', (group) => {
+  let user: any;
+  group.each.setup(async () => {
+    user = await User.create({
+      fullName: 'testuser',
+      email: 'test@example.com',
+      password: 'password'
+    });
+  })
+
+  group.each.teardown(async () => {
+    await user.delete();
+  })
+
+  test('Testa se get /api/products/ retorna o status 200: ', async ({ client }) => {
+    const loginResponse = await client
+    .post('/login')
+    .json({
+      email: 'test@example.com',
+      password: 'password'
+    })
+    
+    const token = loginResponse.body().token;
+
+    const response = await client
+    .get('/api/products/')
+    .header('Authorization', `Bearer ${token}`)
+
+    response.assertStatus(200);
+  })
+
+  test('Testa se get /api/products/ retorna o status 401 ao tentar fazer a requisição com um token invalido: ', async ({ client }) => {
+    await client
+    .post('/login')
+    .json({
+      email: 'test@example.com',
+      password: 'password'
+    })
+
+    const response = await client
+    .get('/api/products/')
+    .header('Authorization', `Bearer umtokenqualquer`);
+
+    response.assertStatus(401);
+  })
 })
