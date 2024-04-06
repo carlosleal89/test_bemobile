28e89a336ac9a3c47ac85810e902aa219024e1c8
Carlos Leal <carlos.frleal@gmail.com>
2024-04-06 17:10:40 -0300

    refactor: created a file for tests of products route

---
+++ tests/functional/products/products.spec.ts
@@ -0,0 +1,49 @@
+import { test } from '@japa/runner'
+import User from '../../../app/models/user.js';
+
+test.group('Testes para as rotas de produtos: ', (group) => {
+  let user: any;
+  group.each.setup(async () => {
+    user = await User.create({
+      fullName: 'testuser',
+      email: 'test@example.com',
+      password: 'password'
+    });
+  })
+
+  group.each.teardown(async () => {
+    await user.delete();
+  })
+
+  test('Testa se get /api/products/ retorna o status 200: ', async ({ client }) => {
+    const loginResponse = await client
+    .post('/login')
+    .json({
+      email: 'test@example.com',
+      password: 'password'
+    })
+    
+    const token = loginResponse.body().token;
+
+    const response = await client
+    .get('/api/products/')
+    .header('Authorization', `Bearer ${token}`)
+
+    response.assertStatus(200);
+  })
+
+  test('Testa se get /api/products/ retorna o status 401 ao tentar fazer a requisição com um token invalido: ', async ({ client }) => {
+    await client
+    .post('/login')
+    .json({
+      email: 'test@example.com',
+      password: 'password'
+    })
+
+    const response = await client
+    .get('/api/products/')
+    .header('Authorization', `Bearer umtokenqualquer`);
+
+    response.assertStatus(401);
+  })
+})
